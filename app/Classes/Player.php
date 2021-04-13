912034241bb4830a56a4d32dd6c091d8a0322f37
fmillox <frederic.millox@gmail.com>
2021-04-13 08:35:23 +0200

    Player class created

---
+++ app/Classes/Player.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace App\Classes;
+
+class Player
+{
+  /**
+   * Represents the name of the player
+   * 
+   * @var string
+   */
+  private $name;
+
+  /**
+   * Represents the deck of the player
+   * 
+   * @var Deck
+   */
+  private $deck;
+
+  /**
+   * Represents the visible card of the player
+   * 
+   * @var Card
+   */
+  private $visibleCard;
+
+  /**
+   * Represents the score of the player
+   * 
+   * @var int
+   */
+  private $score;
+
+  /**
+   * Constructor
+   *
+   * @param string $name The name of the player
+   */
+  public function __construct(string $name)
+  {
+    $this->name = $name;
+    $this->deck = new Deck();
+    $this->visibleCard = null;
+    $this->score = 0;
+  }
+
+  /**
+   * Get the string description of the player
+   *
+   * @return string
+   */
+  public function toString()
+  {
+    $strDeck = $this->deck->toString();
+    if (strlen($strDeck) === 0) {
+      $strDeck = 'vide';
+    }
+
+    return 'Joueur : '.$this->name."\n"
+          .'--- jeu de cartes : '.$strDeck."\n";
+  }
+
+  /**
+   * Get the string description of the player during a round
+   *
+   * @return string
+   */
+  public function roundToString()
+  {
+    $strDeck = $this->deck->toString();
+    if (strlen($strDeck) > 0) {
+      $strDeck = ' ('.$strDeck.')';
+    }
+
+    return 'Joueur : '.$this->name."\n"
+          .'--- carte visible sur la table : '.($this->isCardVisible() ? $this->visibleCard->toString() : 'aucune')."\n"
+          .'--- '.$this->deck->getCount().' carte(s) restante(s)'.$strDeck."\n"
+          .'--- score : '.$this->score."\n";
+  }
+
+  /**
+   * Reset the visible card
+   *
+   * @return void
+   */
+  public function resetVisibleCard()
+  {
+    $this->visibleCard = null;
+  }
+
+  /**
+   * Check if the card is visible
+   *
+   * @return boolean
+   */
+  private function isCardVisible()
+  {
+    return $this->visibleCard !== null;
+  }
+
+  /**
+   * Increase the score of the player
+   *
+   * @param integer $number The number to add to the score (by default, 1)
+   * @return void
+   */
+  public function increaseScore(int $number = 1)
+  {
+    $this->score += $number;
+  }
+
+  /**
+   * Get the value of name
+   *
+   * @return  string
+   */ 
+  public function getName()
+  {
+    return $this->name;
+  }
+
+  /**
+   * Get the value of deck
+   *
+   * @return  Deck
+   */ 
+  public function getDeck()
+  {
+    return $this->deck;
+  }
+
+  /**
+   * Get the value of visibleCard
+   *
+   * @return  Card
+   */ 
+  public function getVisibleCard()
+  {
+    return $this->visibleCard;
+  }
+
+  /**
+   * Set the value of visibleCard
+   *
+   * @param  Card  $visibleCard
+   *
+   * @return  self
+   */ 
+  public function setVisibleCard(?Card $visibleCard)
+  {
+    $this->visibleCard = $visibleCard;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of score
+   *
+   * @return  int
+   */ 
+  public function getScore()
+  {
+    return $this->score;
+  }
+}
+
+?>
--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once('./vendor/autoload.php');
 
 use App\Classes\Card;
 use App\Classes\Deck;
+use App\Classes\Player;
 
 $deck = new Deck();
 echo 'Deck is empty : '.($deck->isEmpty() === true)."\n\n";
@@ -30,4 +31,28 @@ echo 'Nouvelle carte ajoutée : '."\n".$deck->toString()."\n\n";
 
 echo $deck->removeCard($deck->getFirstCard())->toString()."\n";
 echo 'Première carte retirée : '."\n".$deck->toString()."\n\n";
+
+$player = new Player('Toto');
+echo 'Affichage du joueur : '."\n".$player->toString()."\n\n";
+echo 'Affichage du round du joueur : '."\n".$player->roundToString()."\n\n";
+
+$card = $deck->getFirstCard();
+$player->getDeck()->addCard($card);
+$deck->removeCard($card);
+$card = $deck->getFirstCard();
+$player->getDeck()->addCard($card);
+$deck->removeCard($card);
+echo 'Affichage du joueur : '."\n".$player->toString()."\n\n";
+echo 'Affichage du round du joueur : '."\n".$player->roundToString()."\n\n";
+
+$card = $player->getDeck()->getFirstCard();
+$player->getDeck()->removeCard($card);
+$player->setVisibleCard($card);
+$player->increaseScore();
+echo 'Affichage du joueur : '."\n".$player->toString()."\n\n";
+echo 'Affichage du round du joueur : '."\n".$player->roundToString()."\n\n";
+
+$player->resetVisibleCard();
+echo 'Affichage du joueur : '."\n".$player->toString()."\n\n";
+echo 'Affichage du round du joueur : '."\n".$player->roundToString()."\n\n";
 ?>
